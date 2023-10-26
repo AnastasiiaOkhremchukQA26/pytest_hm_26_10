1dd5fab5147183e359853989c6aa2fdad9be3580
AnastasiiaOkhremchukQA26 <nastia.o0403@gmail.com>
2023-10-26 17:07:44 +0100

    init

--- test_hm_26_10.py
+++ test_hm_26_10.py
@@ -1,22 +1,6 @@
 import pytest
 from selenium import webdriver
 
-def pytest_addoption(parser):
-    parser.addoption('--browser_mode', action='store', default="headless",
-                     help="By default is headless mode, but you can set --browser_mode='gui'")
-
-# Параметризована фікстура для старту та закриття браузера
-@pytest.fixture
-def browser(request):
-    browser_mode = request.config.getoption("--browser_mode")
-    if browser_mode == "gui":
-        driver = webdriver.Chrome()
-    elif browser_mode == "headless":
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        driver = webdriver.Chrome(options=options)
-    yield driver
-    driver.quit()
 
 # Перший тест
 @pytest.mark.addition
